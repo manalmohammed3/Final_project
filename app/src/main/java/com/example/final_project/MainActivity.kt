b5556e3e15810131cd0947625fa1fff16261ef3d
manal mohammed <manal.mohammed.ruh.k@tuwaiq.edu.sa>
2021-12-23 11:45:06 +0300

    login feture

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.final_project
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.button)
+
+        val signInLauncher = registerForActivityResult(FirebaseAuthUIActivityResultContract())
+        { res ->
+            this.onSignInResult(res)
+        }
+
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            button.setOnClickListener {
+                signInLauncher.launch(signInIntent)
+                 button.text = "SignIn"
+//                var intent =Intent(this,ListFragment::class.java)
+//                startActivity(intent)
+
+            }
+        } else {
+            button.text = "SignOut"
+            button.setOnClickListener { AuthUI.getInstance().signOut(this) }
+        }
+    }
+
+
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+
+        } else {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        }
     }
 }
